a738d8a1b1ef92dd8683344a0309b7710de208f1
siddharthanand <siddharth.a@captainfresh.in>
2024-01-03 15:56:06 +0530

    intial draft

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useRef } from "react";
+import Users from "./data_models/users.json";
+
+
+// would have implemented debouncing if the data was given through api to limit to number of api calls on search
+// would have given a background to the text with @ in it
+// would have looked into more edge cases like: if @ is removed the list should also disappear
+// would have used reducers for state managemnet of the list
+// would have used better naming convention
 
 function App() {
+  const [activeList, setActiveList] = useState(Users);
+  const [showList, setShowList] = useState(false);
+  const inputRef = useRef();
+
+  const onTextType = (text) => {
+    let n = text.split("@");
+    if (
+      text.split(" ")[text.split(" ").length - 1][0] == "@" &&
+      showList == false
+    ) {
+      setShowList(true);
+    }
+    if (showList == true) {
+      let list = Users.filter((i) => {
+        let str = `${i.first_name} ${i.last_name}`.toLowerCase();
+        if (str.includes(n[n.length - 1].toLowerCase())) {
+          return i;
+        }
+      });
+      setActiveList([...list]);
+    }
+  };
+  const onSelectUser = (item) => {
+    setShowList(false);
+    setActiveList(Users);
+    let n = inputRef.current.value.split("@");
+    let text = `${n[0]}`;
+    n.map((item, index) => {
+      if (index < n.length - 1 && index > 0) text = text + `@${item}`;
+    });
+    inputRef.current.value = `${text}@${item.first_name} ${item.last_name}`;
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <input
+        ref={inputRef}
+        style={{ width: "100%" }}
+        type="text"
+        placeholder="Mention"
+        onChange={(e) => onTextType(e.target.value)}
+      />{" "}
+      {showList ? (
+        <div
+          style={{
+            border: "1px solid black",
+            maxWidth: "200px",
+            width: "auto",
+            marginTop: "10px",
+            padding: "5px",
+            overflowX: "scroll",
+            overflowY: "scroll",
+            maxHeight: "300px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {activeList.map((item, index) => {
+            if (index < 1000) {
+              return (
+                <div
+                  key={item.id}
+                  className="user"
+                  onClick={() => onSelectUser(item)}
+                >
+                  {item.first_name} {item.last_name}
+                </div>
+              );
+            }
+          })}
+        </div>
+      ) : null}
     </div>
   );
 }
